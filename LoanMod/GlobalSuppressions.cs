8537919d6fd50a36d1a6fc140951dbf8f39d2fbe
Dominik H <dcdominoes@gmail.com>
2022-05-11 23:23:28 +0100

    Added chinese translations

--- LoanMod/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Redundancy", "RCS1163:Unused parameter.", Justification = "<Pending>", Scope = "member", Target = "~M:LoanMod.ModEntry.ProcessBorrowing(System.Int32,System.Int32,StardewValley.Farmer,System.String)")]
