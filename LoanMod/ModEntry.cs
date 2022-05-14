e6330cf6b978e22696d3a5c7858d62a0b5a42e1b
Dominik H <dcdominoes@gmail.com>
2022-05-14 10:43:12 +0100

    Fixed issues with repay custom amount

--- LoanMod/ModEntry.cs
+++ LoanMod/ModEntry.cs
@@ -15,7 +15,6 @@ namespace LoanMod
         private int amount, duration;
         private float interest;
         private LoanManager loanManager;
-        private int DailyAmountBeforeCustomPayment;
         private readonly List<LoanMPMessage> mpMessage = new();
 
         private void OnGameLaunched(object sender, GameLaunchedEventArgs e) => AddModFunctions();
@@ -95,7 +94,7 @@ namespace LoanMod
             loanManager.Duration = duration;
             loanManager.Interest = interest;
             loanManager.Balance = (int)loanManager.CalculateBalance;
-            loanManager.DailyAmount = (int)loanManager.CalculateDailyAmount;
+            loanManager.DailyAmount = (int)loanManager.CalculateInitDailyAmount;
 
             Monitor.Log($"Amount: {option}, Duration: {duration}, Interest: {interest}.", LogLevel.Info);
 
@@ -138,9 +137,7 @@ namespace LoanMod
                             loanManager.AmountRepaid += val;
                             loanManager.Balance -= val;
                             //recalculate daily amount in case balance is lower than daily repayment
-                            if (DailyAmountBeforeCustomPayment == 0)
-                                DailyAmountBeforeCustomPayment = loanManager.DailyAmount;
-                            loanManager.DailyAmount = Math.Max(loanManager.DailyAmount - val, 0);
+                            loanManager.AmountRepaidToday += val;
                             AddMessage(I18n.Msg_Payment_Complete(val.ToString("N0")), HUDMessage.achievement_type);
                         }
                         Game1.activeClickableMenu = null;
@@ -163,15 +160,16 @@ namespace LoanMod
                 } //Check if you are still in loan contract
                 else if (loanManager.Balance > 0)
                 {
+                    int moneyToRepay = loanManager.CalculateAmountToPayToday;
                     //If player has enough Money for the daily deduction amount
-                    if (Game1.player.Money >= loanManager.DailyAmount)
+                    if (Game1.player.Money >= moneyToRepay)
                     {
                         //Checks if the balance is greater than or equal to the daily repayment amount
-                        if (loanManager.Balance > loanManager.DailyAmount)
+                        if (loanManager.Balance > moneyToRepay)
                         {
-                            Game1.player.Money -= loanManager.DailyAmount;
-                            loanManager.AmountRepaid += loanManager.DailyAmount;
-                            loanManager.Balance -= loanManager.DailyAmount;
+                            Game1.player.Money -= moneyToRepay;
+                            loanManager.AmountRepaid += moneyToRepay;
+                            loanManager.Balance -= moneyToRepay;
                         }
                         else
                         {
@@ -194,12 +192,12 @@ namespace LoanMod
                             AddMessage(I18n.Msg_Payment_Failed(loanManager.DailyAmount.ToString("N0")), HUDMessage.error_type);
                             if (loanManager.LateDays == 0)
                             {
-                                Game1.addHUDMessage(new HUDMessage(I18n.Msg_Payment_Missed1(loanManager.LateChargeAmount.ToString("N0")), HUDMessage.error_type));
+                                AddMessage(I18n.Msg_Payment_Missed1(loanManager.LateChargeAmount.ToString("N0")), HUDMessage.error_type);
                                 loanManager.LateDays++;
                             }
                             else
                             {
-                                Game1.addHUDMessage(new HUDMessage(I18n.Msg_Payment_Missed2(loanManager.LateChargeAmount.ToString("N0")), HUDMessage.error_type));
+                                AddMessage(I18n.Msg_Payment_Missed2(loanManager.LateChargeAmount.ToString("N0")), HUDMessage.error_type);
                                 loanManager.Balance += loanManager.LateChargeAmount;
                             }
                         }
@@ -217,13 +215,14 @@ namespace LoanMod
             //checks if player is currently taking any loans, if so it will load all the loan data.
             if (Game1.player.IsMainPlayer)
             {
-                loanManager = this.Helper.Data.ReadSaveData<LoanManager>("Doomnik.MoneyManage");
+                loanManager = this.Helper.Data.ReadSaveData<LoanManager>("Doomnik.MoneyManage"); 
             }
 
             if (loanManager == null || Config.Reset)
             {
                 loanManager = new LoanManager();
                 Config.Reset = false;
+                Helper.WriteConfig<ModConfig>(Config);
                 AddMessage(I18n.Msg_Create(), HUDMessage.achievement_type);
             }
         }
@@ -236,15 +235,12 @@ namespace LoanMod
                 if (loanManager.HasPaid)
                 {
                     if (loanManager.DailyAmount > 0) 
-                        AddMessage(I18n.Msg_Payment_Complete(loanManager.DailyAmount.ToString("N0")), HUDMessage.achievement_type);
-                    if (DailyAmountBeforeCustomPayment > 0)
-                    {
-                        loanManager.DailyAmount = DailyAmountBeforeCustomPayment;
-                        DailyAmountBeforeCustomPayment = 0;
-                    }
+                        AddMessage(I18n.Msg_Payment_Complete(loanManager.CalculateAmountToPayToday.ToString("N0")), HUDMessage.achievement_type);
+
+                    loanManager.AmountRepaidToday = 0;
                     loanManager.HasPaid = false;
                 }
-                if (loanManager.Balance < loanManager.DailyAmount) { loanManager.DailyAmount = loanManager.Balance; }
+                if (loanManager.Balance < loanManager.DailyAmount) loanManager.DailyAmount = loanManager.Balance;
             }
             else
             {
@@ -264,7 +260,7 @@ namespace LoanMod
         /// </summary>
         private void DayEnding(object sender, DayEndingEventArgs e)
         {
-            if (!Context.IsMainPlayer)
+            if (!Context.IsMainPlayer && Context.IsMultiplayer)
             {
                 var m = new LoanMPMessage
                 {
